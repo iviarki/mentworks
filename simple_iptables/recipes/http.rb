0f53c595ea668741c74e9ace3ef604f1f8f08124
iviarki <iviarki@gmail.com>
2014-05-05 08:19:55 +0000

    Replaced simple_iptables with iptables

--- simple_iptables/recipes/http.rb
+++
@@ -1,6 +0,0 @@
-# Allow HTTP, HTTPS
-simple_iptables_rule "http" do
-  rule [ "--proto tcp --dport 80",
-    "--proto tcp --dport 443" ]
-  jump "ACCEPT"
-end
--- simple_iptables/recipes/ssh.rb
+++
@@ -1,5 +0,0 @@
-# Allow SSH
-simple_iptables_rule "ssh" do
-  rule "--proto tcp --dport 22"
-  jump "ACCEPT"
-end
