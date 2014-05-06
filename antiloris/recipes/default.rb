f4605d189d5270ee92a6991428e81197f65df43d
iviarki <iviarki@gmail.com>
2014-05-06 09:50:12 +0000

    Mods

--- antiloris/recipes/default.rb
+++ antiloris/recipes/default.rb
@@ -1,8 +1,18 @@
 package 'httpd-devel'
+
 cookbook_file '/usr/local/src/mod_antiloris.c' do
 	source 'mod_antiloris.c'
 end
+
+execute "LoadModule" do
+     command "echo \"#LoadModule foo_module /usr/lib/apache2/modules/foo.so\" >> #{node[:apache][:dir]}/conf/httpd.conf"
+end
+
 execute "Install mod_antiloris" do
 	cwd "/usr/local/src"
 	command "apxs -a -i -c mod_antiloris.c"
+end
+
+execute "Restart Apache" do
+    command "service httpd restart"
 end
